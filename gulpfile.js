09f344b8f998f2b60ed00a5af285d01fde3b5321
Zack Penka <zpenka@gmail.com>
2016-01-10 10:20:15 -0500

    Remove minify script, Update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
-// Build script
+// lint script
 import gulp from 'gulp';
-import uglify from 'gulp-uglify';
 import eslint from 'gulp-eslint';
-
-gulp.task('minify', ['lint'], () => {
-  return gulp.src('js/dist/sequence.js')
-         .pipe(uglify())
-         .pipe(rename('js/dist/bundle.mins.js'))
-         .pipe(gulp.dest('js'));
-});
 
 gulp.task('lint', () => {
   return gulp.src(['**/*.js', '!node_modules/**'])
@@ -21,4 +13,4 @@ gulp.task('dev', ['lint'], ()=> {
   return gulp.watch(['js/**/*.js', '!js/dist/*.js'], ['lint']);
 });
 
-gulp.task('default', ['minify']);
+gulp.task('default', ['dev']);
